fda8e37ada9809d1a3ee35b57759d47b5757c5f0
James Cooper <30380166+jcoo092@users.noreply.github.com>
2017-09-07 11:32:29 +1200

    Update tsp.fs
    Updated the F# version to the latest.  Main big change is that it now uses proper e records, instead of an adjacency list.

--- tsp.fs
+++ tsp.fs
@@ -1,24 +1,36 @@
-let infty = 10000
-
-let E = // Specification of set of E objects
-   [|  [| 0; 1; 3; infty; 2; |];
-       [| 1; 0; infty; 6; 4; |];
-       [| 3; infty; 0; 8; 5; |];
-       [| infty; 6; 8; 0; 7; |];
-       [| 2; 4; 5; 7; 0; |];
-   |]
-
+type e = {f: int; t: int; c: int} // e(f, t, w)
 type s = int * Set<int> * list<int> * int // (r, u, p, c)
 type z = list<int> * int // (p, c)
+
+let E = [|
+    {f = 1; t = 2; c = 1};
+    {f = 1; t = 3; c = 3};
+    {f = 1; t = 5; c = 2};
+    {f = 2; t = 1; c = 1};
+    {f = 2; t = 4; c = 6};
+    {f = 2; t = 5; c = 4};
+    {f = 3; t = 1; c = 3};
+    {f = 3; t = 4; c = 8};
+    {f = 3; t = 5; c = 5};
+    {f = 4; t = 2; c = 6};
+    {f = 4; t = 3; c = 8};
+    {f = 4; t = 5; c = 7};
+    {f = 5; t = 1; c = 2};
+    {f = 5; t = 2; c = 4};
+    {f = 5; t = 3; c = 5};
+    {f = 5; t = 4; c = 7}
+|]
 
 let snd4 (_, b, _, _) = b
 
 let visit (si:s) (u:Set<int>): list<s> =
-    let r, _, f::p, c = si
-    [for t in u do
-        let w = E.[f].[t]
-        if w < infty then
-            yield (r, Set.remove t u, t :: f :: p, c + w) ]
+    let r, _, fx::p, c = si
+    [for tx in u do
+        let edge = Array.choose (fun x -> match x with
+                                          | {f = f'; t = t'; c = W} when f' = fx && t' = tx -> Some(x)
+                                          | _ -> None) E
+        if Array.length edge > 0 then
+            yield (r, Set.remove tx u, tx :: fx :: p, c + edge.[0].c) ]
 
 let rec sgoal (slist: list<s>): list<s> =
     let r, u, _, _ = List.head slist
@@ -32,8 +44,8 @@ let minh (h: list<z>): z =
     h |> List.minBy (fun (p, c) -> c)
     
 let go () = 
-    let r = 0
-    let y = set [0..(Array.length E - 1)] |> Set.remove r
-    hgoal r y |> (fun s -> printfn "%A" s; s) |> minh |> printfn "%A"
+    let r = 1
+    let v = set [1..5] |> Set.remove r
+    hgoal r v |> (fun s -> printfn "%A" s; s) |> minh |> printfn "%A"
 
 go ()
